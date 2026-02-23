8e7dc5fbc28fc0112d807931359de97432a34bbc
bedel <lemotiobedel@gmail.com>
2026-02-23 18:40:59 +0100

    canTerminer

--- folio.ts
+++ folio.ts
@@ -1,26 +1,10 @@
-private readonly checkTacheTrigger$ = this.state$.pipe(
-  map(s => ({
-    entiteId: s.selectedEntiteId,
-    compteId: s.selectedCompteId
-  })),
-  filter(v => !!v.entiteId && !!v.compteId),
-  distinctUntilChanged(
-    (a, b) =>
-      a.entiteId === b.entiteId &&
-      a.compteId === b.compteId
-  )
-);
-
-readonly checkTacheEffect$ = this.runEffect(
-  this.checkTacheTrigger$,
-  ({ entiteId, compteId }) =>
-    this.conciliationRepository
-      .verifierTacheEnCours(entiteId!, compteId!)
-      .pipe(
-        tap(result => {
-          this.patchState({
-            canTerminerConciliation: result
-          });
-        })
-      )
-);
+const canTerminer$ =
+      modeTravail === 'ENTITE'
+        // ENTITE -> API comptes -> vérifier que compteId est présent via identifiantCompteGL
+        ? this.obtenirComptesGLAvecTachesNonTerminees(entiteId).pipe(
+            map((comptes) => comptes.some((c) => c.identifiantCompteGL === compteId))
+          )
+        // COMPTE -> API transits -> vérifier que entiteId est présent via identifiantTransit
+        : this.obtenirTransitsAvecTachesNonTerminees(compteId).pipe(
+            map((transits) => transits.some((t) => t.identifiantTransit === entiteId))
+          );
