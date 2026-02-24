24285b4cb793fe2bb8132b6dda5d846d8e46749f
bedel <lemotiobedel@gmail.com>
2026-02-24 21:12:07 +0100

    defaultColDef

--- folio.ts
+++ folio.ts
@@ -1,34 +1,12 @@
-private readonly canTerminerConciliation$: Observable<boolean> = this.runEffect<boolean>(
-    this.readyTriggers$.pipe(
-      switchMap(({ entiteId, compteId, modeTravail }) => {
-        if (modeTravail === 'ENTITE') {
-          return this.tacheConciliationAutoRepo
-            .obtenirComptesGLAvecTachesNonTerminees({ idTransit: entiteId })
-            .pipe(
-              map(comptes => comptes?.some(c => (c.identifiantCompteGL ?? '') === compteId) ?? false)
-            );
-        }
+H CAD, [Feb 24, 2026 at 9:10:37 PM]:
+{
+  headerName: 'Date',
+  field: 'date',
+  cellClass: 'text-center'
+}
 
-        return this.tacheConciliationAutoRepo
-          .obtenirTransitsAvecTachesNonTerminees({ idCompteGL: compteId })
-          .pipe(
-            map(transits => transits?.some(t => (t.identifiantTransit ?? '') === entiteId) ?? false)
-          );
-      })
-    ),
-    (err) => err as AppError,
-    {
-      fallbackValue: false,
-      useGlobalSpinner: true,
-      clearAlertOnStart: true,
-      error: {
-        title: 'Chargement impossible',
-        fallbackMessage: 'Impossible de vérifier les tâches en cours',
-      },
-    }
-  ).pipe(
-    // si entité/compte se vident, readyTriggers$ n’émet plus, donc canTerminer reste sur l’ancienne valeur.
-    // => startWith(false) garantit que l’écran démarre à false.
-    startWith(false),
-    shareReplay({ bufferSize: 1, refCount: true })
-  );
+
+defaultColDef: {
+  cellClass: 'text-center',
+  headerClass: 'header-center'
+}
